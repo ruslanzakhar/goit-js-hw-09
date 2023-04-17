3d351479841ea7ae39e03ecad1e23c746ca278eb
ruslanzakhar <115725965+ruslanzakhar@users.noreply.github.com>
2023-04-17 15:31:55 +0000

    Deploying to gh-pages from @ ruslanzakhar/goit-js-hw-09@64a7ecb1b565c0991a0d854e3b5a04c0777379db 🚀

---
+++ 01-color-switcher.56d1f137.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body;t.addEventListener("click",(function(n){a=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0}),1e3)})),n.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1}));var a=null}();
+//# sourceMappingURL=01-color-switcher.56d1f137.js.map
